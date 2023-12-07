69d0568e9b3c92d2f38fd5c73c2b81c2ecebe456
9thCircle <the9thcircleofhelll@gmail.com>
2023-12-07 22:35:00 +0300

    Added troubleshooting

--- HeartAttackPage.xaml.cs
+++ HeartAttackPage.xaml.cs
@@ -12,22 +12,46 @@ public partial class HeartAttackPage : ContentPage
 
     private void FirstEtr_Completed(object sender, EventArgs e)
     {
-
+        SubmitBtn_Clicked(null, null);
     }
 
     private void ChestPainBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-
+        SubmitBtn_Clicked(null, null);
     }
 
     private void SubmitBtn_Clicked(object sender, EventArgs e)
     {
+        HealthData healthData;
+        try
+        {
+            healthData = new(ChestPainBox.IsChecked);
+        }
+        catch (ArgumentNullException)
+        {
+            PredictionLbl.Text = "One or more boxes is empty.";
+            return;
+        }
+        catch (ArgumentException ex)
+        {
+            PredictionLbl.Text = ex.Message;
+            return;
+        }
+
         MLContext context = new();
-        ITransformer model = context.Model.Load(File.Open("", FileMode.Open), out _);
+
+        ITransformer model;
+        try
+        {
+            model = context.Model.Load(File.Open("", FileMode.Open), out _);
+        }
+        catch (Exception ex)
+        {
+            PredictionLbl.Text = ex.Message;
+            return;
+        }
+
         var predictionEngine = context.Model.CreatePredictionEngine<HealthData, OutData>(model);
-
-
-        HealthData healthData = new(ChestPainBox.IsChecked);
 
         var prediction = predictionEngine.Predict(healthData);
 
