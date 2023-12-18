bb7e7bef4214106d942e46121567d0eab898135a
9thCircle <the9thcircleofhelll@gmail.com>
2023-12-18 22:56:27 +0300

    Oops

--- HeartAttackPage.xaml.cs
+++ HeartAttackPage.xaml.cs
@@ -38,70 +38,60 @@ public partial class HeartAttackPage : ContentPage
             DisplayAlert("Error", "Please enter a valid age.", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(sex) || (sex.ToUpper() != "M" && sex.ToUpper() != "F"))
         {
             // Handle the case where sex is not reasonable
             DisplayAlert("Error", "Please enter a valid sex (M or F).", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(chestPainType))
         {
             // Handle the case where chestPainType is not reasonable
             DisplayAlert("Error", "Please enter a valid chest pain type.", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(restingBP) || !float.TryParse(restingBP, out _))
         {
             // Handle the case where restingBP is not reasonable
             DisplayAlert("Error", "Please enter a valid resting blood pressure.", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(cholesterol) || !float.TryParse(cholesterol, out _))
         {
             // Handle the case where cholesterol is not reasonable
             DisplayAlert("Error", "Please enter a valid serum cholesterol.", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(restingECG))
         {
             // Handle the case where restingECG is not reasonable
             DisplayAlert("Error", "Please enter valid resting ECG results.", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(maxHR) || !int.TryParse(maxHR, out int maxHRValue) || maxHRValue < 60 || maxHRValue > 202)
         {
             // Handle the case where maxHR is not reasonable
             DisplayAlert("Error", "Please enter a valid maximum heart rate (between 60 and 202).", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(exerciseAngina) || (exerciseAngina.ToUpper() != "Y" && exerciseAngina.ToUpper() != "N"))
         {
             // Handle the case where exerciseAngina is not reasonable
             DisplayAlert("Error", "Please enter a valid exercise-induced angina (Y or N).", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(oldpeak) || !double.TryParse(oldpeak, out _))
         {
             // Handle the case where oldpeak is not reasonable
             DisplayAlert("Error", "Please enter a valid oldpeak value.", "OK");
             return;
         }
-
         if (string.IsNullOrWhiteSpace(stSlope))
         {
             // Handle the case where stSlope is not reasonable
             DisplayAlert("Error", "Please enter a valid ST slope.", "OK");
             return;
         }
-
 
         ProcessStartInfo startInfo = new()
         {
@@ -111,7 +101,7 @@ public partial class HeartAttackPage : ContentPage
             RedirectStandardError = true,
             UseShellExecute = false,
             CreateNoWindow = true,
-            Arguments = $"C:\\Users\\SUPER-LAPTOP\\source\\repos\\Other\\Other\\HyperData\\bin\\Debug\\net7.0-windows10.0.19041.0\\win10-x64\\model.py {age} {sex == "M"} {chestPainType} {restingBP} {cholesterol} {fastingBS} {restingECG} {maxHR} {exerciseAngina} {oldpeak} {stSlope}"
+            Arguments = $"[pathToFile.py] {age} {sex == "M"} {chestPainType} {restingBP} {cholesterol} {fastingBS} {restingECG} {maxHR} {exerciseAngina} {oldpeak} {stSlope}"
         };
 
         // Create and start the process
